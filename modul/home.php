d76665f7d25930101c0d8c48b1b776e63f6f5da3
omattt <85796311+omattt@users.noreply.github.com>
2021-06-16 10:42:42 +0700

    Add files via upload

---
+++ modul/home.php
@@ -0,0 +1,7 @@
+<div class="jumbotron mt-3">
+  <h1 class="display-4">Selamat Datang Admin,</h1>
+  <p class="lead">E-Arsip adalah program yang akan memudahkan anda dalam mengarsip surat.</p>
+  <hr class="my-4">
+  <p>Anda dapat menggunakan menu-menu yang ada diatas, terimakasih.</p>
+  <a class="btn btn-danger btn-lg" href="logout.php" role="button">Logout</a>
+</div>
